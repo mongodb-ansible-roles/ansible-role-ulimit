c54f2121fac4853c8b8d81e11e6222fad2abcb98
John Chen <johnchen456@gmail.com>
2019-08-23 16:23:04 -0400

    test: fixed rubocop brackets error

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -5,10 +5,10 @@ describe file('/etc/security/limits.conf') do
 end
 
 describe limits_conf('/etc/security/limits.conf') do
-  its('*') { should include %w(hard nofile 64000) }
-  its('*') { should include %w(soft nofile 64000) }
-  its('*') { should include %w(hard nproc 64000) }
-  its('*') { should include %w(soft nproc 64000) }
-  its('*') { should include %w(hard memlock 1024) }
-  its('*') { should include %w(soft memlock 1024) }
+  its('*') { should include %w[hard nofile 64000] }
+  its('*') { should include %w[soft nofile 64000] }
+  its('*') { should include %w[hard nproc 64000] }
+  its('*') { should include %w[soft nproc 64000] }
+  its('*') { should include %w[hard memlock 1024] }
+  its('*') { should include %w[soft memlock 1024] }
 end
